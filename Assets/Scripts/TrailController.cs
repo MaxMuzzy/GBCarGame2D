9bd4b69339265f6b3be3ca855eb7988806d5c711
MaxMuzzy <m.a.saltykov@gmail.com>
2022-08-27 21:31:02 +0300

    Done homework for lesson 1

---
+++ Assets/Scripts/TrailController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailController : BaseController
+{
+    public TrailController()
+    {
+        _view = LoadView();
+        _view.Init();
+    }
+
+    private readonly ResourcePath _viewPath = new ResourcePath { PathResource = "Prefabs/Trail" };
+    private TrailView _view;
+
+    private TrailView LoadView()
+    {
+        var objView = Object.Instantiate(ResourceLoader.LoadPrefab(_viewPath));
+        AddGameObjects(objView);
+
+        return objView.GetComponent<TrailView>();
+    }
+}
